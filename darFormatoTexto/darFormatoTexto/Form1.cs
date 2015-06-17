d453d43ab572c9b47290ef05db5b4c674c091e09
Albert Duran <albertrdg@gmail.com>
2015-06-17 17:29:06 -0400

    Aplicacion para dar formato a un texto dado

---
+++ darFormatoTexto/darFormatoTexto/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace darFormatoTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            if (color.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.ForeColor = color.Color;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FontDialog font = new FontDialog();
+            font.Font = richTextBox1.Font;
+            if (font.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Font = font.Font;
+            }
+        }
+    }
+}
